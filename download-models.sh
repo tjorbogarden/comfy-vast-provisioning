deeb3c8865478b41de53925d27f9f7e8bbc1cbf5
tjorbogarden <162017715+tjorbogarden@users.noreply.github.com>
2024-03-07 13:23:07 +0100

    Update download-models.sh

--- download-models.sh
+++ download-models.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+# Link to download script: https://github.com/tjorbogarden/comfy-vast-provisioning/raw/main/download-models.sh
 # Run the script by typing ./download-models.sh in the right directory
 
 # Directory where files will be downloaded
