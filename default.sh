b4b085005423138843b77e35ead08268f1294f4c
tjorbogarden <162017715+tjorbogarden@users.noreply.github.com>
2024-03-11 21:12:55 +0100

    Update default.sh

--- default.sh
+++ default.sh
@@ -173,7 +173,7 @@ function provisioning_start() {
         "${WORKSPACE}/storage/stable_diffusion/models/embeddings" \
         "${EMBEDDINGS[@]}"
     provisioning_get_models \
-        "${WORKSPACE}/opt/ComfyUI/pysssss-workflows" \
+        "${WORKSPACE}/ComfyUI/pysssss-workflows" \
         "${WORKFLOWS[@]}"
     provisioning_print_end
 }
