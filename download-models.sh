49a92a0a25a7d03eb1e2d054c51e8af384a11a4c
tjorbogarden <162017715+tjorbogarden@users.noreply.github.com>
2024-03-11 19:24:15 +0100

    Update download-models.sh

--- download-models.sh
+++ download-models.sh
@@ -32,14 +32,63 @@ urls=(
     #  Pony Diffusion V6
     #"https://civitai.com/api/download/models/290640?token=ad415154d4ad70c87127eb1bbe7bc6b4"
     #  Suzannes Mix
-    "https://civitai.com/api/download/models/312314?token=ad415154d4ad70c87127eb1bbe7bc6b4"
-
+    #"https://civitai.com/api/download/models/312314?token=ad415154d4ad70c87127eb1bbe7bc6b4"
 
     # SUPIR upscale model
-    #"https://huggingface.co/camenduru/SUPIR/resolve/main/SUPIR-v0Q.ckpt"
+    "https://huggingface.co/camenduru/SUPIR/resolve/main/SUPIR-v0Q.ckpt"
 
     #CCSR Upscale model
-    #"https://huggingface.co/camenduru/CCSR/resolve/main/real-world_ccsr.ckpt"
+    "https://huggingface.co/camenduru/CCSR/resolve/main/real-world_ccsr.ckpt"
+
+    #  Stable Cascade
+    "https://huggingface.co/stabilityai/stable-cascade/resolve/main/stage_a.safetensors"
+    "https://huggingface.co/stabilityai/stable-cascade/resolve/main/comfyui_checkpoints/stable_cascade_stage_b.safetensors"
+    "https://huggingface.co/stabilityai/stable-cascade/resolve/main/comfyui_checkpoints/stable_cascade_stage_c.safetensors"
+)
+
+# Loop through the URLs and download each one
+for url in "${urls[@]}"; do
+    wget -P "$DOWNLOAD_DIR" "$url" --content-disposition
+done
+
+# Directory where files will be downloaded
+DOWNLOAD_DIR="/opt/ComfyUI/models/lora/"
+
+# Create the download directory if it doesn't exist
+mkdir -p "$DOWNLOAD_DIR"
+
+# List of URLs to download
+urls=(
+    #Add Details (Detail Tweaker XL)
+    #"https://civitai.com/api/download/models/135867?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    #  Sexy
+    #"https://civitai.com/api/download/models/186014?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    #  Silhouette - Sliders
+    #"https://civitai.com/api/download/models/262669?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    #  Harness
+    #"https://civitai.com/api/download/models/216424?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    #  Pinups style Art Frahm
+    #"https://civitai.com/api/download/models/202646?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    # Woman nude 
+    #"https://civitai.com/api/download/models/363218?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    #  Rembrandt Lighting
+    #"https://civitai.com/api/download/models/315808?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    #  Underwear 
+    #"https://civitai.com/api/download/models/136277?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    #  Flash Photo
+    #"https://civitai.com/api/download/models/203379?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    #  Low Key Lighting
+    #"https://civitai.com/api/download/models/315777?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    #  High Key Lighting 
+    #"https://civitai.com/api/download/models/315742?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    # Chiaroscuro Lighting Contrasting Lighting
+    "https://civitai.com/api/download/models/315825?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    #  Bad Quality
+    #"https://civitai.com/api/download/models/311787?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    #  Impossible Photos
+    "https://civitai.com/api/download/models/343625?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    # Perfect Hands
+    "https://civitai.com/api/download/models/254267?token=ad415154d4ad70c87127eb1bbe7bc6b4"
 )
 
 # Loop through the URLs and download each one
