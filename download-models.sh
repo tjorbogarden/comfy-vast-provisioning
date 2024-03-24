6f6041c31432b1950fa4dfc38174150fd8f4de25
tjorbogarden <162017715+tjorbogarden@users.noreply.github.com>
2024-03-24 15:21:19 +0100

    Update download-models.sh

--- download-models.sh
+++ download-models.sh
@@ -49,6 +49,10 @@ urls=(
     "https://civitai.com/api/download/models/282430?token=ad415154d4ad70c87127eb1bbe7bc6b4"
     # litho orwo dokumentpapier
     "https://civitai.com/api/download/models/404096?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    # Lingquivera
+    "https://civitai.com/api/download/models/281935?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    #Mentixis
+    "https://civitai.com/api/download/models/315064?token=ad415154d4ad70c87127eb1bbe7bc6b4"
 )
 
 # Loop through the URLs and download each one
