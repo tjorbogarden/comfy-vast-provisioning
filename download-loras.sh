33a2e1a83ce451199be520e10170aa986ff8df07
tjorbogarden <162017715+tjorbogarden@users.noreply.github.com>
2024-03-26 11:30:26 +0100

    Update download-loras.sh

--- download-loras.sh
+++ download-loras.sh
@@ -54,6 +54,8 @@ urls=(
     "https://civitai.com/api/download/models/237604?token=ad415154d4ad70c87127eb1bbe7bc6b4"
     # Double Vision
     "https://civitai.com/api/download/models/132469?token=ad415154d4ad70c87127eb1bbe7bc6b4"
+    # Dystopia
+    "https://civitai.com/api/download/models/332104?token=ad415154d4ad70c87127eb1bbe7bc6b4"
 )
 
 # Loop through the URLs and download each one
