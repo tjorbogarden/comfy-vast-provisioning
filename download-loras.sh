83060a7dd4c99c3e64ba3a79704e843810366efc
tjorbogarden <162017715+tjorbogarden@users.noreply.github.com>
2024-03-26 11:50:21 +0100

    Update download-loras.sh

--- download-loras.sh
+++ download-loras.sh
@@ -60,5 +60,5 @@ urls=(
 
 # Loop through the URLs and download each one
 for url in "${urls[@]}"; do
-    wget -P "$DOWNLOAD_DIR" "$url" --content-disposition
+    wget -P -qnc --content-disposition --show-progress "$DOWNLOAD_DIR" "$url"
 done
