f4789172a930b3561cf5e237c7e8ba6b2a3d00d7
tjorbogarden <162017715+tjorbogarden@users.noreply.github.com>
2024-06-13 16:39:15 +0200

    Update pixart.sh

--- pixart.sh
+++ pixart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Link to download script: wget https://github.com/tjorbogarden/comfy-vast-provisioning/raw/main/dl-loras.sh --content-disposition
-# Make script executable: chmod +x download-loras.sh
-# Run the script by typing bash ./download-loras.sh (in the right directory)
+# Link to download script: wget https://github.com/tjorbogarden/comfy-vast-provisioning/raw/main/pixart.sh --content-disposition
+# Make script executable: chmod +x pixart.sh
+# Run the script by typing bash ./pixart.sh (in the right directory)
 
 # Directory where files will be downloaded
 DOWNLOAD_DIR="/opt/ComfyUI/models/checkpoints/"
@@ -16,6 +16,8 @@ urls=(
     "https://huggingface.co/PixArt-alpha/PixArt-alpha/resolve/main/PixArt-XL-2-1024-MS.pth"
     "https://huggingface.co/PixArt-alpha/PixArt-Sigma/resolve/main/PixArt-Sigma-XL-2-1024-MS.pth"
     "https://huggingface.co/PixArt-alpha/PixArt-LCM-XL-2-1024-MS/resolve/main/transformer/diffusion_pytorch_model.safetensors"
+    #Juggernaut SDXL
+    "https://huggingface.co/RunDiffusion/Juggernaut-X-v10/resolve/main/Juggernaut-X-RunDiffusion-NSFW.safetensors"
 )
 
 # Loop through the URLs and download each one
