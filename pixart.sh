86f549f96035d312d14faecf7de4ae5bf4d51d58
tjorbogarden <162017715+tjorbogarden@users.noreply.github.com>
2024-06-13 16:52:30 +0200

    Update pixart.sh

--- pixart.sh
+++ pixart.sh
@@ -15,7 +15,7 @@ urls=(
     #Pixart Sigma 1024
     "https://huggingface.co/PixArt-alpha/PixArt-alpha/resolve/main/PixArt-XL-2-1024-MS.pth"
     "https://huggingface.co/PixArt-alpha/PixArt-Sigma/resolve/main/PixArt-Sigma-XL-2-1024-MS.pth"
-    "https://huggingface.co/PixArt-alpha/PixArt-LCM-XL-2-1024-MS/resolve/main/transformer/diffusion_pytorch_model.safetensors"
+    
     #Juggernaut SDXL
     "https://huggingface.co/RunDiffusion/Juggernaut-X-v10/resolve/main/Juggernaut-X-RunDiffusion-NSFW.safetensors"
 )
@@ -24,6 +24,25 @@ urls=(
 for url in "${urls[@]}"; do
     wget -qnc --content-disposition --show-progress -P "$DOWNLOAD_DIR" "$url"
 done
+
+# Directory where files will be downloaded
+DOWNLOAD_DIR="/opt/ComfyUI/models/vae/"
+
+# Create the download directory if it doesn't exist
+mkdir -p "$DOWNLOAD_DIR"
+
+# List of URLs to download
+urls=(
+    #Pixart VAE
+    "https://huggingface.co/PixArt-alpha/PixArt-LCM-XL-2-1024-MS/resolve/main/transformer/diffusion_pytorch_model.safetensors"
+)
+
+# Loop through the URLs and download each one
+for url in "${urls[@]}"; do
+    wget -qnc --content-disposition --show-progress -P "$DOWNLOAD_DIR" "$url"
+done
+#rename file
+mv "$DOWNLOAD_DIR"+diffusion_pytorch_model.safetensors pixart_sigma_vae.safetensors
 
 # Directory where files will be downloaded
 DOWNLOAD_DIR="/opt/ComfyUI/models/t5/"
