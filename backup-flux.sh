23fb61bc14dbb48f5fc64395e6b7f464c60b8e21
tjorbogarden <162017715+tjorbogarden@users.noreply.github.com>
2024-12-30 13:57:09 +0100

    Update backup-flux.sh

--- backup-flux.sh
+++ backup-flux.sh
@@ -8,12 +8,12 @@ ZIPFILE="flux-backup.zip"
 
 # Specify the folders you want to include in the zip
 FOLDERS=(
-    "opt/ComfyUI/custom_nodes"
-    "opt/ComfyUI/models/loras/"
-    "opt/ComfyUI/models/pulid"
-    "opt/ComfyUI/models/configs"
-    "opt/ComfyUI/user/default/workflows"
-    "opt/ComfyUI/models/clip/"
+    "/opt/ComfyUI/custom_nodes"
+    "/opt/ComfyUI/models/loras/"
+    "/opt/ComfyUI/models/pulid"
+    "/opt/ComfyUI/models/configs"
+    "/opt/ComfyUI/user/default/workflows"
+    "/opt/ComfyUI/models/clip/"
 )
 
 # Specify patterns/files/folders you want to exclude
@@ -22,8 +22,8 @@ FOLDERS=(
 # - "folder2/private/*" excludes everything in /path/to/folder2/private
 # - "folder1/secret.txt" excludes exactly secret.txt in folder1
 EXCLUDES=(
-    "opt/ComfyUI/models/clip/t5xxl_fp16.safetensors"
-    "opt/ComfyUI/models/clip/clip_l.safetensors"
+    "/opt/ComfyUI/models/clip/t5xxl_fp16.safetensors"
+    "/opt/ComfyUI/models/clip/clip_l.safetensors"
 )
 
 # Zip the folders recursively, applying the exclude patterns
