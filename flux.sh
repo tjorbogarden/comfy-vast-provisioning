15e54a66ca4a068ce37b0b753979d3b6704b77d3
tjorbogarden <162017715+tjorbogarden@users.noreply.github.com>
2025-01-01 17:44:48 +0100

    Update flux.sh

--- flux.sh
+++ flux.sh
@@ -23,6 +23,10 @@ pip install openai==1.55.3 httpx==0.27.2 --force-reinstall --quiet
 # install prereq joytag
 pip install lxml bitsandbytes
 
+# Face Recognintion
+apt install cmake
+pip install face_recognition
+
 
 # List of URLs to download for unet
 unet_urls=(
